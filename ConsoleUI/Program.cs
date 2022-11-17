aa38f43b2f2eb68ead2533b9df72c3094e421bc7
hasanyannar <hasanyannar@gmail.com>
2022-11-17 12:40:10 +0300

    Core Katmanı Eklendi

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,9 +9,9 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new EfProductDal());
+            ProductManager productManager = new ProductManager(new EfProductDal()); 
 
-            foreach (var product in productManager.GetByUnitPrice(50,100))
+            foreach (var product in productManager.GetAll())
             {
                 Console.WriteLine(product.ProductName);
 
