c86a2032a7b2f840f9ba3158142bfaaa4b66bb41
hasanyannar <hasanyannar@gmail.com>
2022-11-21 15:28:06 +0300

    ResultMessage

---
+++ Core/Utilities/Results/DataResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class DataResult<T> : Result, IDataResult<T>
+    {
+        public DataResult(T data, bool suceess, string message):base(suceess,message)
+        {
+            Data = data;
+        }
+        public DataResult(T data, bool suceess):base(suceess)
+        {
+            Data = data;
+        }
+
+        public T Data { get; }
+    }
+}
